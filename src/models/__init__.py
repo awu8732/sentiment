4d6550e8af0ba095ed573805c8dbe970997861de
Alex Wu <alex.wu1288@gmail.com>
2025-08-18 20:44:01 -0500

    move sentiment_features into features folder

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,4 +1,4 @@
 from .article import NewsArticle
 from .stock import StockData
-from .sentiment_features import SentimentFeatures
+from .features.sentiment_features import SentimentFeatures
 from .features.market_features import MarketFeatures
